e5152a7b23804b34530f06aaf513cce03a21d3aa
Andrew Gibson <a.gibson@n2y.com>
2025-08-20 14:31:10 +0100

    removed capa

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.flightnav.app',
-  appName: 'flight-nav',
-  webDir: 'out'
-};
-
-export default config;
